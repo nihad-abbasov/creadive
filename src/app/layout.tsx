955781049e0d2fcb58fc45ae8299100325dab0c1
Nihad Abbasov <nihad.abbasov.work@gmail.com>
2025-06-20 00:51:14 +0400

    added open graph and etc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,29 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "Creadive - İdeallarınızı gerçəkləşdirək ✨",
   description:
-    "Biz veb inkişaf, dizayn və rəqəmsal marketinq həlləri sahəsində ixtisaslaşmış yaradıcı rəqəmsal agentliyik.",
+    "Biz vebsayt yaradılması, dizayn və rəqəmsal marketinq həlləri sahəsində ixtisaslaşmış yaradıcı rəqəmsal agentliyik.",
+  openGraph: {
+    title: "Creadive - İdeallarınızı gerçəkləşdirək ✨",
+    description:
+      "Biz vebsayt yaradılması, dizayn və rəqəmsal marketinq həlləri sahəsində ixtisaslaşmış yaradıcı rəqəmsal agentliyik.",
+    type: "website",
+    url: "https://creadive.az",
+    images: [
+      {
+        url: "https://creadive.az/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Creadive - İdeallarınızı gerçəkləşdirək ✨",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Creadive - İdeallarınızı gerçəkləşdirək ✨",
+    description:
+      "Biz vebsayt yaradılması, dizayn və rəqəmsal marketinq həlləri sahəsində ixtisaslaşmış yaradıcı rəqəmsal agentliyik.",
+    images: ["https://creadive.az/og-image.png"],
+  },
 };
 
 export default function RootLayout({
