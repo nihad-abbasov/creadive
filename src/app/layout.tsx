f49ba3e6f7b38a8ba294227d387eaed749bdbff7
Nihad Abbasov <n.abbasov@xalqsigorta.az>
2025-08-21 16:56:06 +0400

    turned back rooutlayout canonical

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,7 +106,9 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/logos/white_h.png" />
         <link rel="mask-icon" href="/logos/white_h.svg" color="#1e40af" />
 
-        {/* Canonical URL - will be set per page */}
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://creadive.az" />
+        
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
