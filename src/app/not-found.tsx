8d82c00e26c6852c86283a10889173d78888d040
Nihad Abbasov <n.abbasov@xalqsigorta.az>
2025-08-22 11:43:35 +0400

    meta tags added to NotFound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,22 @@
+import DynamicMetaTags from "@/components/DynamicMetaTags";
 import NotFoundView from "@/views/NotFoundView";
 
 export default function NotFound() {
-  return <NotFoundView />;
+  return (
+    <>
+      <DynamicMetaTags
+        title="404 - Səhifə tapılmadı | Creadive - İdealarınızı gerçəkləşdirək ✨"
+        description="Səhifə tapılmadı. Düzgün URL-i yoxlayın."
+        canonical="https://creadive.az/404"
+        ogTitle="404 - Səhifə tapılmadı"
+        ogDescription="Səhifə tapılmadı. Düzgün URL-i yoxlayın."
+        ogUrl="https://creadive.az/404"
+        ogImage="https://creadive.az/og-img.png"
+        twitterTitle="404 - Səhifə tapılmadı"
+        twitterDescription="Səhifə tapılmadı. Düzgün URL-i yoxlayın."
+        twitterImage="https://creadive.az/og-img.png"
+      />
+      <NotFoundView />
+    </>
+  );
 } 
